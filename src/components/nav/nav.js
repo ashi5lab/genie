31decb516141fd28bb51c147b8b37f848a9a1d30
Ashique <ashiqpkodiyathur@gmail.com>
2023-09-20 18:32:54 +0530

    testing

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -14,7 +14,7 @@ function Headernav() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Test</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
